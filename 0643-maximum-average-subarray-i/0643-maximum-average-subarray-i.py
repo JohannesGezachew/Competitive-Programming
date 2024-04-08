be4da0ece44efdcc06ee637218fb25de28211762
Johng899 <97839635+Johng899@users.noreply.github.com>
2024-04-09 00:37:20 +0300

    Time: 880 ms (73.58%), Space: 28.2 MB (96.89%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.py
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.py
@@ -3,7 +3,7 @@ class Solution:
         window_sum = sum(nums[:k])
         n = len(nums)
         max_sum = window_sum
-        for i in range(n-k):
-            window_sum = window_sum - nums[i] + nums[i+k]
+        for i in range(n - k):
+            window_sum = window_sum - nums[i] + nums[i + k]
             max_sum = max(window_sum, max_sum)
-        return max_sum /k
+        return max_sum / k
