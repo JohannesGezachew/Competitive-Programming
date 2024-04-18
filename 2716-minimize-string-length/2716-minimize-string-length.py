7d0622d116d77eb89555e81b54f28e3f2ae56db3
Johng899 <97839635+Johng899@users.noreply.github.com>
2024-04-18 23:55:24 +0300

    Time: 37 ms (96.24%), Space: 16.5 MB (54.15%) - LeetHub

--- 2716-minimize-string-length/2716-minimize-string-length.py
+++ 2716-minimize-string-length/2716-minimize-string-length.py
@@ -1,9 +1,3 @@
 class Solution:
     def minimizedStringLength(self, s: str) -> int:
-        mini_string_len = len(s)
-        a =Counter(s)
-        keys = [i for i in a.values()]
-        for key in keys:
-            if key >= 2:
-                mini_string_len -= key -1
-        return(mini_string_len)
+        return(len(set(s)))
