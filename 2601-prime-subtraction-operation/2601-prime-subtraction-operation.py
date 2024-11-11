1fc4023db88a49b43ba6e588c1e85e3afceb7c0c
JohannesGezachew <97839635+JohannesGezachew@users.noreply.github.com>
2024-11-12 00:18:16 +0300

    Time: 37 ms (53.8%), Space: 16.8 MB (36.36%) - LeetHub

---
+++ 2601-prime-subtraction-operation/2601-prime-subtraction-operation.py
@@ -0,0 +1,24 @@
+class Solution:
+    def check_prime(self, x: int) -> bool:
+        if x < 2:
+            return False
+        for i in range(2, int(x**0.5) + 1):
+            if x % i == 0:
+                return False
+        return True
+
+    def primeSubOperation(self, nums: List[int]) -> bool:
+        for i in range(len(nums)):
+            bound = nums[0] if i == 0 else nums[i] - nums[i - 1]
+            if bound <= 0:
+                return False
+
+            largest_prime = 0
+            for j in range(bound - 1, 1, -1):
+                if self.check_prime(j):
+                    largest_prime = j
+                    break
+
+            nums[i] -= largest_prime
+
+        return True
