14d362a49967e5cd6c9260adcb25d9d9c35893ce
JohannesGezachew <97839635+JohannesGezachew@users.noreply.github.com>
2024-11-20 16:09:25 +0300

    Time: 40 ms (54.21%), Space: 17 MB (21.68%) - LeetHub

--- 1754-largest-merge-of-two-strings/1754-largest-merge-of-two-strings.py
+++ 1754-largest-merge-of-two-strings/1754-largest-merge-of-two-strings.py
@@ -1,16 +1,19 @@
 class Solution:
     def largestMerge(self, word1: str, word2: str) -> str:
-        word1 = list(word1)
-        word2 = list(word2)
-        merge = []*(len(word1)+len(word2))
-        
-        while word1 and word2:
-            if word1 > word2:
-                merge.append(word1.pop(0))
+        merged_word = []
+        left, right = 0, 0
+
+        while left < len(word1) and right < len(word2):
+            if word1[left:] > word2[right:]:
+                merged_word.append(word1[left])
+                left += 1
             else:
-                merge.append(word2.pop(0))
-        
-        merge.extend(word1)
-        merge.extend(word2)
-        
-        return "".join(merge)
+                merged_word.append(word2[right])
+                right += 1
+
+        if left < len(word1):
+            merged_word.extend(word1[left:])
+        if right < len(word2):
+            merged_word.extend(word2[right:])
+
+        return "".join(merged_word)
