98a6a9a10c465405b68889d755ca98f873c74192
JohannesGezachew <97839635+JohannesGezachew@users.noreply.github.com>
2024-11-30 14:24:55 +0300

    Time: 55 ms (5.26%), Space: 20 MB (18.12%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -1,17 +1,11 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        count = 0
-        left = 0
-        window_sum = 0
-        
+        curr_sum = 0
+        counter = Counter([0])
+        count  = 0
         for right in range(len(nums)):
-            window_sum += nums[right]
-            
-            while window_sum > k and left < right:
-                window_sum -= nums[left]
-                left += 1
-            
-            if window_sum == k:
-                count += 1
-        
+            curr_sum += nums[right]
+            if curr_sum - k in counter:
+                count += counter[curr_sum - k]
+            counter[curr_sum] += 1
         return count
