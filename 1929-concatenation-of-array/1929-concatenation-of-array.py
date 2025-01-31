12425c506f9e5c776dc663898ffe208723fd014d
JohannesGezachew <97839635+JohannesGezachew@users.noreply.github.com>
2025-01-31 11:59:35 +0300

    Time: 0 ms (100%), Space: 12.6 MB (82.26%) - LeetHub

--- 1929-concatenation-of-array/1929-concatenation-of-array.py
+++ 1929-concatenation-of-array/1929-concatenation-of-array.py
@@ -1,5 +1,9 @@
-class Solution:
-    def getConcatenation(self, nums: List[int]) -> List[int]:
-        num_copy = nums
-        num_copy.extend(nums)
-        return num_copy
+class Solution(object):
+    def getConcatenation(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        nums.extend(nums)
+        return nums
+        
